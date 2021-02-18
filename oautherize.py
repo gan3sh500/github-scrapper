27aaf4c488a9f61f850a5ccf36efa44cc61817bb
Mazhar Shaikh <mazharshaikh94@gmail.com>
2021-02-18 20:15:53 +0900

    Authorization Workflow working

--- oautherize.py
+++ oautherize.py
@@ -1,8 +1,9 @@
 import json
 import requests
+import config 
 
 data = {
-    'client_id': 'here'
+    'client_id': config.client_id,
 }
 
 def make_json(text):
@@ -18,4 +19,21 @@ def make_json(text):
     return json_dict
 
 response = requests.post('https://github.com/login/device/code', data=data)
-print(make_json(response.text))
+json = make_json(response.text)
+print(json)
+x = input()
+data = {
+    'client_id': config.client_id,
+    'client_secret': config.client_secret,
+    'device_code': config.device_code,
+    'grant_type': 'urn:ietf:params:oauth:grant-type:device_code'
+}
+response = requests.post('https://github.com/login/oauth/access_token', data=data)
+json = make_json(response.text)
+print(json)
+for i in range(100):
+    auth = ('username', config.authorization_code)
+    response = requests.get('https://api.github.com/users/octocat', auth=auth)
+    # print(json.loads(response.text))
+    print(i)
+
