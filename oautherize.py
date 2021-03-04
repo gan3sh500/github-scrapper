d2b13a19c4c2f8bcec5688b087786f50b145fe46
ganesh anand <gan3sh500@gmail.com>
2021-03-04 18:02:55 +0530

    streamlined oauth workflow

--- oautherize.py
+++ oautherize.py
@@ -1,39 +1,65 @@
 import json
-import requests
-import config 
+import requests 
+from urllib.parse import unquote
 
-data = {
-    'client_id': config.client_id,
-}
+import webbrowser
+from cselector import yes_or_no
 
-def make_json(text):
-    text_list = text.split('&')
-    json_dict = {}
-    for item in text_list:
-        k, v = item.split('=')
-        if '%3A' in v:
-            v = v.replace('%3A', ':')
-        if '%2F' in  v:
-            v = v.replace('%2F', '/')
-        json_dict[k] = v
-    return json_dict
 
-response = requests.post('https://github.com/login/device/code', data=data)
-json = make_json(response.text)
-print(json)
-x = input()
-data = {
-    'client_id': config.client_id,
-    'client_secret': config.client_secret,
-    'device_code': config.device_code,
+def read_json(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+
+def dump_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+def get_device_code(config):
+    data = {
+        'client_id': config["client_id"],
+    }
+    headers = {
+        "accept": "application/json",
+    }
+    response = requests.post('https://github.com/login/device/code', data=data, headers=headers)
+    response_json = response.json()
+    print(f"User Code : {response_json['user_code']}")
+    webbrowser.open(response_json["verification_uri"], new=2)
+    ret = yes_or_no(question="Did you authorize the device?",default="y")
+    if ret:
+        config["device_code"] = response_json["device_code"]
+    else:
+        raise ValueError('Not authorized.')
+    return config
+
+
+def get_authorization_code(config):
+    data = {
+    'client_id': config["client_id"],
+    'device_code': config["device_code"],
     'grant_type': 'urn:ietf:params:oauth:grant-type:device_code'
-}
-response = requests.post('https://github.com/login/oauth/access_token', data=data)
-json = make_json(response.text)
-print(json)
-for i in range(100):
-    auth = ('username', config.authorization_code)
-    response = requests.get('https://api.github.com/users/octocat', auth=auth)
-    # print(json.loads(response.text))
-    print(i)
+    }
+    headers = {
+        "accept": "application/json",
+    }
+    response = requests.post('https://github.com/login/oauth/access_token', data=data, headers=headers)
+    response_json = response.json()
+    config["authorization_code"] = response_json["access_token"]
+    return config
 
+
+def main():
+    config = read_json("config.json")
+    if "client_id" not in config:
+        raise ValueError("No client id in config.json.")
+    if "device_code" not in config:
+        config = get_device_code(config)
+    if "authorization_code" not in config:
+        config = get_authorization_code(config)
+    dump_json(config, "config.json")
+    print("Authorization ready.")
+
+if __name__ == '__main__':
+    main()
